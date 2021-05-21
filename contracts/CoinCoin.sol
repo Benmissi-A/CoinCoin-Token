bf14d641575adfc47429066ce4bc8008d6b61cef
Benmissi-A <aminebenmissi@hotmail.fr>
2021-05-21 17:17:24 +0200

    CoinCoin-test

--- contracts/CoinCoin.sol
+++ contracts/CoinCoin.sol
@@ -3,12 +3,17 @@
 pragma solidity ^0.8.0;
 
 contract CoinCoin {
+
   mapping(address => uint256) private _balances;
+  mapping(address => mapping(address => uint256)) private _allowances;
   string private _name;
   string private _symbol;
   uint256 private _totalSupply;
 
   event Transfer(address indexed sender, address indexed receipient, uint256 amount);
+  event Transfered(address indexed sender,address indexed recipient, uint256 amount);
+  event TransferedFrom(address indexed from,address indexed to, uint256 amount);
+  event Approval(address indexed sender, address indexed spender, uint256 amount);
 
   constructor(address owner_, uint256 totalSupply_) {
     _name = "CoinCoin";
@@ -29,9 +34,37 @@ contract CoinCoin {
     return _totalSupply;
   }
 
-  function balanceOf(address account) public view returns (uint256) {
-    return _balances[account];
+  function balanceOf(address account_) public view returns (uint256) {
+    return _balances[account_];
   }
 
+  function allowance(address owner_, address spender_) public view returns (uint256) {
+    return _allowances[owner_][spender_];
+  }
+
+  function approve(address sender , address spender, uint256 amount) public  {
+    require(sender != address(0), "CoinCoin: approve from the zero address");
+    require(spender != address(0), "CoinCoin: approve to the zero address");
+
+    _allowances[sender][spender] = amount;
+    emit Approval(sender, spender, amount);
+  }
+
+  function transfer(address sender_ ,address recipient_, uint256 amount_) public {
+    require( amount_ > 0, "CoinCoin: can not transfer 0 ether"  );
+    require( _balances[sender_] >= amount_,"CoinCoin: Not enough Ether to transfer");
+    require( recipient_ != address(0),"CoinCoin: transfer to the zero address" );
+    _balances[sender_] -= amount_;
+    _balances[recipient_] += amount_;
+    emit Transfered(sender_, recipient_, amount_);
+  }
+
+  function transferFrom( address from_, address to_,uint256 amount_ ) public {
+    require(_allowances[from_][to_] >= amount_, "CoinCoin: You don't have any funds allowed anymore.");
+    _allowances[from_][to_] -= amount_;
+    _balances[from_] -= amount_;
+    _balances[to_] += amount_;
+    emit TransferedFrom(from_, to_, amount_);
+  }
 
 }
--- test/CoinCoin-test.js
+++ test/CoinCoin-test.js
@@ -7,16 +7,15 @@ describe('CoinCoin Token', function () {
   const INITIAL_SUPPLY = ethers.utils.parseEther('8000000000')
 
   beforeEach(async function () {
-    ;[dev, owner, alice, bob, charlie, dan, eve] = await ethers.getSigners()
+    [dev, owner, alice, bob, charlie, dan, eve] = await ethers.getSigners()
     CoinCoin = await ethers.getContractFactory('CoinCoin')
     coincoin = await CoinCoin.connect(dev).deploy(owner.address, INITIAL_SUPPLY)
     await coincoin.deployed()
     /*
-    Il faudra transférer des tokens à nos utilisateurs de tests lorsque la fonction transfer sera implementé
+    Il faudra transférer des tokens à nos utilisateurs de tests lorsque la fonction transfer sera implementé*/
     await coincoin
       .connect(owner)
       .transfer(alice.address, ethers.utils.parseEther('100000000'))
-      */
   })
 
   describe('Deployement', function () {
@@ -37,19 +36,34 @@ describe('CoinCoin Token', function () {
     it('emits event Transfer when mint totalSupply', async function () {
       /*
       Pb de récupération de l'event d'une transaction passée avec Waffle: Sofiane s'en occupe
+     
       await expect(
         CoinCoin.connect(dev).deploy(
           owner.address,
           ethers.utils.parseEther('8000000000')
         )
       ).to.emit(, 'Transfer').withArgs(ethers.constants.AddressZero, owner.address, ethers.utils.parseEther('8000000000'));
-      */
+       */
     })
   })
 
-  describe('Allowance system', function () {
-    // Tester le système d'allowance ici
-  })
+  //   describe('balances system', function () {
+  //   // Tester le système d'allowance ici
+  //   it('approve from sender to spender', async function () {
+  //     expect(await coincoin.balanceOf(alice.address)).to.equal(100000000)
+  //   })
+  //   // it('get allowance fro sender', async function () {
+  //   // })
+  // })
+
+  // describe('Allowance system', function () {
+  //   // Tester le système d'allowance ici
+  //   it('approve from sender to spender', async function () {
+  //     await coincoin.approve(alice.address,charlie.address,1000000)
+  //     console.log(alice.addres)
+  //     expect(await coincoin.allowance(alice.address,charlie.address)).to.equal(1000000)
+  //   })
+  // })
   describe('Token transfers', function () {
     it('transfers tokens from sender to receipient', async function () {})
     it('transferFrom tokens from sender to receipient', async function () {})
